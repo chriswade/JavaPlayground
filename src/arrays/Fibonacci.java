5ff99a832eee9e68748f74a5d9a731bd0c3cade5
Christopher Wade <chris.wade@comparethemarket.com.au>
2019-11-27 15:10:56 +1000

    init commit

---
+++ src/arrays/Fibonacci.java
@@ -0,0 +1,31 @@
+package arrays;
+
+public class Fibonacci {
+    public static String main(int n) {
+        String fib = "";
+        int first = 0, second = 1, fibonacci;
+        System.out.println("arrays.Fibonacci to the value of " + n + " terms");
+        for(int c = 0; c < n; c++) {
+            if(c <= 1) {
+                fibonacci = c;
+                fib += fibonacci + " ";
+            } else {
+                fibonacci = first + second;
+                first = second;
+                second = fibonacci;
+                fib += fibonacci + " ";
+            }
+            System.out.println(fibonacci + " ");
+        }
+        return fib;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Fibonacci.main(10));
+    }
+
+}
+
+
+
+

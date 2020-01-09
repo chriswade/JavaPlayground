9d4f84901b4ec09960d3b9a280ecc6b7262bb642
Christopher Wade <chris.wade@comparethemarket.com.au>
2020-01-09 16:56:32 +1000

    added

---
+++ src/loops/MultiplicationTable.java
@@ -0,0 +1,23 @@
+package loops;
+
+class MultiplicationTable {
+    public static String test(int num)
+    {
+        String table = "";
+        int i = 1;
+        int k = 10;
+        while (k > 0) {
+            int answer = num * i;
+            System.out.println(num + " x " + i + " = " + answer);
+            table += String.valueOf(answer) + " ";
+            i++;
+            k--;
+        }
+        return table;
+    }
+
+    public static void main(String[] args) {
+        String answer = test(9);
+        System.out.println(answer);
+    }
+}

ebe4f280179e2ae17d33ffe899a28089d72d00cb
Christopher Wade <chris.wade@comparethemarket.com.au>
2020-01-10 10:06:48 +1000

    moved fibonacci to loops folder

--- src/loops/Fibonacci.java
+++ src/loops/Fibonacci.java
@@ -1,10 +1,10 @@
-package arrays;
+package loops;
 
 public class Fibonacci {
     public static String main(int n) {
         String fib = "";
         int first = 0, second = 1, fibonacci;
-        System.out.println("arrays.Fibonacci to the value of " + n + " terms");
+        System.out.println("loops.Fibonacci to the value of " + n + " terms");
         for(int c = 0; c < n; c++) {
             if(c <= 1) {
                 fibonacci = c;
